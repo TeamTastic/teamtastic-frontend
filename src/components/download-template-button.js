ecfcae40edf742254ba02da95171dc3345c748e0
David <dvazquez3@correo.um.edu.uy>
2024-04-04 16:38:19 -0300

    template page

---
+++ src/components/download-template-button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './download-template-button.css'; // Importamos los estilos CSS
+
+function DownloadTemplateButton({ onClick, children }) {
+  return (
+    <button className='download-template-button download-template-button--gooey' onClick={onClick}>
+      {children}
+      <div className='download-template-button__blobs'>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </button>
+  );
+}
+
+export default DownloadTemplateButton;
