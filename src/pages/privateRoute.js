e02c69cb36af60170c4ff0c0da3f90b46555220f
David <dvazquez3@correo.um.edu.uy>
2024-04-09 21:18:14 -0300

    Update privateRoute.js

--- src/pages/privateRoute.js
+++ src/pages/privateRoute.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axiosConfig';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function PrivateRoute() {
   const [message, setMessage] = useState('');
@@ -9,11 +11,14 @@ function PrivateRoute() {
     axios.get('/private_route')
       .then(response => {
         // Si la solicitud es exitosa, mostrar el mensaje de la ruta privada
+        toast.success('Esto indica que esta bien iniciada la sesión');
         setMessage(response.data);
       })
       .catch(error => {
         // Si hay un error, mostrar el mensaje de error
         setMessage('Error al acceder a la ruta privada');
+        toast.error('Esto indica que esta bien iniciada la sesión');
+
       });
   }, []); // La dependencia vacía asegura que el efecto se ejecute solo una vez al cargar el componente
 
@@ -21,6 +26,7 @@ function PrivateRoute() {
     <div>
       <h1>Ruta Privada</h1>
       <p>{message}</p>
+      <ToastContainer />
     </div>
   );
 }
