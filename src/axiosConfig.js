f74390c56dc5ea0c99bb3a58282327d9de46a08a
bpereda <106450242+bpereda@users.noreply.github.com>
2024-05-18 14:41:47 -0300

    cambio url al back

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://oyster-app-4m62c.ondigitalocean.app/',
+  baseURL: 'https://teamtastic-back-z244p.ondigitalocean.app/',
   withCredentials: true,// Reemplaza con la URL base de tu backend
   // Otras opciones de configuración de Axios si las necesitas
 });
