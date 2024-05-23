5395f33a1a4dde97ec2c45ca72d41e72e54d8470
ValentinAltamirano1 <valtamirano@correo.um.edu.uy>
2024-05-23 00:13:19 -0300

    change test from using placeholder to getByLabelText + add test cases for name and surname

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -12,142 +12,214 @@ const renderWithRouter = (ui, { route = '/' } = {}) => {
 };
 
 describe('Register Component', () => {
-
     test('successful register', async () => {
         renderWithRouter(<Register />);
 
-        const nameInput = screen.getByPlaceholderText('Nombre de Usuario');
-        const emailInput = screen.getByPlaceholderText('Correo electrónico');
-        const passwordInput = screen.getByPlaceholderText('Contraseña');
-        const confirmPasswordInput = screen.getByPlaceholderText('Repetir contraseña');
-  
-        fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+        const surnameInput = screen.getByLabelText(/apellido/i);
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
+
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(surnameInput, { target: { value: 'Doe' } });
+        fireEvent.change(usernameInput, { target: { value: 'johndoe' } });
         fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
         fireEvent.change(passwordInput, { target: { value: 'secretpassword' } });
         fireEvent.change(confirmPasswordInput, { target: { value: 'secretpassword' } });
-  
-        const submitButton = screen.getByText('Registrarse');
+
+        const submitButton = screen.getByText('Enviar');
         fireEvent.click(submitButton);
-  
+
         await waitFor(() => {
             expect(nameInput.validity.valueMissing).toBe(false);
+            expect(surnameInput.validity.valueMissing).toBe(false);
+            expect(usernameInput.validity.valueMissing).toBe(false);
             expect(emailInput.validity.valueMissing).toBe(false);
             expect(passwordInput.validity.valueMissing).toBe(false);
             expect(confirmPasswordInput.validity.valueMissing).toBe(false);
         });
-
     });
- 
-    test('Missing User Name Error', async () => {
 
+    test('Missing Name Error', async () => {
         renderWithRouter(<Register />);
 
-        fireEvent.change(screen.getByPlaceholderText('Correo electrónico'), { target: { value: 'john.doe@example.com' } });
-        fireEvent.change(screen.getByPlaceholderText('Contraseña'), { target: { value: 'secretpassword' } });
-        fireEvent.change(screen.getByPlaceholderText('Repetir contraseña'), { target: { value: 'secretpassword' } });      
+        const surnameInput = screen.getByLabelText(/apellido/i);
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
 
-        const nameInput = screen.getByPlaceholderText('Nombre de Usuario');
+        fireEvent.change(surnameInput, { target: { value: 'Doe' } });
+        fireEvent.change(usernameInput, { target: { value: 'johndoe' } });
+        fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'secretpassword' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'secretpassword' } });
 
-        const submitButton = screen.getByText('Registrarse');
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+
+        const submitButton = screen.getByText('Enviar');
         fireEvent.click(submitButton);
 
-        await waitFor(() =>{
+        await waitFor(() => {
             expect(nameInput.validity.valueMissing).toBe(true);
         });
-        
+    });
+
+    test('Missing User Name Error', async () => {
+        renderWithRouter(<Register />);
+
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+        const surnameInput = screen.getByLabelText(/apellido/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
+
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(surnameInput, { target: { value: 'Doe' } });
+        fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'secretpassword' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'secretpassword' } });
+
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+
+        const submitButton = screen.getByText('Enviar');
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(usernameInput.validity.valueMissing).toBe(true);
+        });
+    });
+
+    test('Missing Surname Error', async () => {
+        renderWithRouter(<Register />);
+
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
+
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(usernameInput, { target: { value: 'johndoe' } });
+        fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'secretpassword' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'secretpassword' } });
+
+        const surnameInput = screen.getByLabelText(/apellido/i);
+
+        const submitButton = screen.getByText('Enviar');
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(surnameInput.validity.valueMissing).toBe(true);
+        });
     });
 
     test('Missing Email Error', async () => {
-
         renderWithRouter(<Register />);
 
-        fireEvent.change(screen.getByPlaceholderText('Nombre de Usuario'), { target: { value: 'John Doe' } });
-        fireEvent.change(screen.getByPlaceholderText('Contraseña'), { target: { value: 'secretpassword' } });
-        fireEvent.change(screen.getByPlaceholderText('Repetir contraseña'), { target: { value: 'secretpassword' } });      
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+        const surnameInput = screen.getByLabelText(/apellido/i);
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
 
-        const emailInput = screen.getByPlaceholderText('Correo electrónico');
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(surnameInput, { target: { value: 'Doe' } });
+        fireEvent.change(usernameInput, { target: { value: 'johndoe' } });
+        fireEvent.change(passwordInput, { target: { value: 'secretpassword' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'secretpassword' } });
 
-        const submitButton = screen.getByText('Registrarse');
+        const emailInput = screen.getByLabelText(/email/i);
+
+        const submitButton = screen.getByText('Enviar');
         fireEvent.click(submitButton);
-  
-        await waitFor(() =>{
+
+        await waitFor(() => {
             expect(emailInput.validity.valueMissing).toBe(true);
         });
-        
     });
 
     test('Missing Password Error', async () => {
-
         renderWithRouter(<Register />);
 
-        fireEvent.change(screen.getByPlaceholderText('Nombre de Usuario'), { target: { value: 'John Doe' } });
-        fireEvent.change(screen.getByPlaceholderText('Correo electrónico'), { target: { value: 'john.doe@example.com' } });
-        fireEvent.change(screen.getByPlaceholderText('Repetir contraseña'), { target: { value: 'secretpassword' } });
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+        const surnameInput = screen.getByLabelText(/apellido/i);
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
 
-        const passwordInput = screen.getByPlaceholderText('Contraseña');
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(surnameInput, { target: { value: 'Doe' } });
+        fireEvent.change(usernameInput, { target: { value: 'johndoe' } });
+        fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'secretpassword' } });
 
-        const submitButton = screen.getByText('Registrarse');
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+
+        const submitButton = screen.getByText('Enviar');
         fireEvent.click(submitButton);
-  
-        await waitFor(() =>{
+
+        await waitFor(() => {
             expect(passwordInput.validity.valueMissing).toBe(true);
         });
-        
     });
 
     test('Missing Password Confirmation Error', async () => {
-
         renderWithRouter(<Register />);
 
-        fireEvent.change(screen.getByPlaceholderText('Nombre de Usuario'), { target: { value: 'John Doe' } });
-        fireEvent.change(screen.getByPlaceholderText('Correo electrónico'), { target: { value: 'john.doe@example.com' } });
-        fireEvent.change(screen.getByPlaceholderText('Contraseña'), { target: { value: 'secretpassword' } });
+        const nameInput = screen.getAllByLabelText(/nombre/i)[0];
+        const surnameInput = screen.getByLabelText(/apellido/i);
+        const usernameInput = screen.getByLabelText(/nombre de usuario/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
 
-        const passwordConfirmationInput = screen.getByPlaceholderText('Repetir contraseña');
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(surnameInput, { target: { value: 'Doe' } });
+        fireEvent.change(usernameInput, { target: { value: 'johndoe' } });
+        fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'secretpassword' } });
 
-        const submitButton = screen.getByText('Registrarse');
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
+
+        const submitButton = screen.getByText('Enviar');
         fireEvent.click(submitButton);
-  
-        await waitFor(() =>{
-            expect(passwordConfirmationInput.validity.valueMissing).toBe(true);
+
+        await waitFor(() => {
+            expect(confirmPasswordInput.validity.valueMissing).toBe(true);
         });
     });
 
     test('Invalid Email Error', async () => {
-
         renderWithRouter(<Register />);
-    
-        const emailInput = screen.getByPlaceholderText('Correo electrónico');
+
+        const emailInput = screen.getByLabelText(/email/i);
         fireEvent.change(emailInput, { target: { value: 'john.doe.example.com' } });
-        
-        const submitButton = screen.getByText('Registrarse');
-        fireEvent.click(submitButton);
 
-        await waitFor(() =>{
-            expect(emailInput.value.includes('@')).toBe(false);
-        });
-        
-    });
-
-    test('Password equals Password Confirmation', async () => {
-        
-        renderWithRouter(<Register />);
-    
-        const passwordInput = screen.getByPlaceholderText('Contraseña');
-        const confirmPasswordInput = screen.getByPlaceholderText('Repetir contraseña');
-    
-        fireEvent.change(passwordInput, { target: { value: 'passwordexample' } });
-        fireEvent.change(confirmPasswordInput, { target: { value: 'passwordexample' } });
-    
-        const submitButton = screen.getByText('Registrarse');
+        const submitButton = screen.getByText('Enviar');
         fireEvent.click(submitButton);
 
         await waitFor(() => {
-                      
-            expect(passwordInput.value).toBe(confirmPasswordInput.value);
-            
+            expect(emailInput.validity.typeMismatch).toBe(true);
         });
     });
-    
 
-}); 
+    test('Password equals Password Confirmation', async () => {
+        renderWithRouter(<Register />);
+
+        const passwordInput = screen.getAllByLabelText(/contraseña/i)[0];
+        const confirmPasswordInput = screen.getByLabelText(/confirma contraseña/i);
+
+        fireEvent.change(passwordInput, { target: { value: 'passwordexample' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'passwordexample' } });
+
+        const submitButton = screen.getByText('Enviar');
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+            expect(passwordInput.value).toBe(confirmPasswordInput.value);
+        });
+    });
+});
