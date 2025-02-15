44afaa71eb4dd5d1f1fafff40e8b2883d935bf2d
David <64923176+DavidVaaa@users.noreply.github.com>
2025-02-15 19:27:31 -0300

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://teamtastic-back-dev-ihnk6.ondigitalocean.app',
+  baseURL: 'https://teamtastic-backend.onrender.com',
   withCredentials: true,// Reemplaza con la URL base de tu backend
   // Otras opciones de configuración de Axios si las necesitas
 });
